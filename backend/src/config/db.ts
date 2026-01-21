e3969aac2ca992f9a5c555b8f405e2704cf63c6f
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2026-01-22 00:43:27 +0530

    Add files via upload

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,1 +1,12 @@
+import { Pool } from "pg";
 
+const pool = new Pool({
+  host: "127.0.0.1",
+  port: 5433,
+  user: "reachinbox",
+  password: "reachinbox",
+  database: "reachinbox_db",
+  ssl: false,
+});
+
+export default pool;
